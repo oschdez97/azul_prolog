28b294fd1b382c284e64e6d1ca5c8a7d3079f72c
oscar <oschdez97@gmail.com>
2020-04-01 14:24:24 -0700

    some changes

--- scr/utils.pl
+++ scr/utils.pl
@@ -1,3 +1,5 @@
+cls :- write('\e[H\e[2J').
+
 indexer(M, I, J, R):-
 	nth1(I, M, T),
 	nth1(J, T, R).
@@ -29,63 +31,75 @@ printMatrix(M, N) :-
 	pMatrix(M, 1, 1, MAX, N).
 
 
-randint(X, Y) :- Y is random(X).
+take(N, _, Xs):-
+	N =< 0, !,
+	N =:=0,
+	Xs = [].
+
+take(_, [], []).
+
+take(N, [X|Xs], [X|Ys]):-
+	M is N - 1,
+	take(M, Xs, Ys).
+
+
+makeRepos(0, _, _, []).
+makeRepos(N, M, S, [X|Y]):-
+	K is N - 1,
+	take(M, S, X),
+	makeRepos(K, M, S, Y).
+
+
+init :- wall(W), stair(S), shuffle(Bag), play(W, S, Bag).
+
+% Este es el hombre, se llama con
+% [Wall, Stair, Bag] que representa cada jugador
+% Se llama a MakeRepos y se actualizan los tableros
+% de cada jugador en funcion de la estrategia que siga cada uno
+% Se llama recursivo a play con los nuevos [Wall, Stair, Bag] y otros
+% argvs necesarios...
+
+play(Wall, Stair, Bag):-
+	printMatrix(Wall, 5),
+	nl,
+	print(Stair),
+	nl,
+	print(Bag).
+
+
+
+stair(X):- X = [[empty],
+	       [empty, empty],
+	       [empty, empty, empty],
+	       [empty, empty, empty, empty],
+	       [empty, empty, empty, empty, empty]].
+
+
+wall(X):- X = [[blue: 0, yellow: 0, red:   0, black: 0, white: 0],
+	      [white: 0, blue:   0, yellow:0, red:   0, black: 0],
+	      [black: 0, white:  0, blue:  0, yellow:0, red:   0],
+	      [red:   0, black:  0, white: 0, blue:  0, yellow:0],
+	      [yellow:0, red:    0, black: 0, white: 0, blue:  0]].
+
+
 
 shuffle(R) :- random_permutation([1, 1, 1, 1, 1, 1,
-				    1, 1, 1, 1, 1, 1,
-				    1, 1, 1, 1, 1, 1,
-				    1, 1, 1, 1, 1, 1,
-				    2, 2, 2, 2, 2, 2,
-				    2, 2, 2, 2, 2, 2,
-				    2, 2, 2, 2, 2, 2,
-				    2, 2, 2, 2, 2, 2,
-                                    3, 3, 3, 3, 3, 3,
-				    3, 3, 3, 3, 3, 3,
-				    3, 3, 3, 3, 3, 3,
-				    3, 3, 3, 3, 3, 3,
-                                    4, 4, 4, 4, 4, 4,
-				    4, 4, 4, 4, 4, 4,
-				    4, 4, 4, 4, 4, 4,
-				    4, 4, 4, 4, 4, 4,
-                                    5, 5, 5, 5, 5, 5,
-				    5, 5, 5, 5, 5, 5,
-				    5, 5, 5, 5, 5, 5,
-				    5, 5, 5, 5, 5, 5], R).
-
-
-
-makeRepos(Y):- shuffle(W), make_Repos(5, W, Y), print(Y).
-make_Repos(0, _, R):-print(R).
-make_Repos(N, [I1, I2, I3, I4|Y], R):-
-	M is N - 1,
-	append(R, [[I1, I2, I3, I4]], W),
-	make_Repos(M, Y, W).
-
-test(X):-make_Repos(2, [1, 2, 3, 4, 5, 6, 7, 8], X).
-
-init :- play([[blue: 0, yellow: 0, red:   0, black: 0, white: 0],
-	     [white: 0, blue:   0, yellow:0, red:   0, black: 0],
-             [black: 0, white:  0, blue:  0, yellow:0, red:   0],
-             [red:   0, black:  0, white: 0, blue:  0, yellow:0],
-             [yellow:0, red:    0, black: 0, white: 0, blue:  0]]
-	     ,
-	     [[empty],
-             [empty, empty],
-             [empty, empty, empty],
-             [empty, empty, empty, empty],
-	     [empty, empty, empty, empty, empty]]).
-
-modify(_, [[1, 2], [3, 4]]).
-
-play(Wall, _):-
-	printMatrix(Wall, 5),
-	modify(Wall, NewWall),
-	write('//////////////////'),
-	nl,
-	printMatrix(NewWall, 2).
-
-cls :- write('\e[H\e[2J').
-
-
-
-
+				  1, 1, 1, 1, 1, 1,
+				  1, 1, 1, 1, 1, 1,
+				  1, 1, 1, 1, 1, 1,
+				  2, 2, 2, 2, 2, 2,
+				  2, 2, 2, 2, 2, 2,
+				  2, 2, 2, 2, 2, 2,
+				  2, 2, 2, 2, 2, 2,
+				  3, 3, 3, 3, 3, 3,
+				  3, 3, 3, 3, 3, 3,
+				  3, 3, 3, 3, 3, 3,
+				  3, 3, 3, 3, 3, 3,
+				  4, 4, 4, 4, 4, 4,
+				  4, 4, 4, 4, 4, 4,
+				  4, 4, 4, 4, 4, 4,
+				  4, 4, 4, 4, 4, 4,
+				  5, 5, 5, 5, 5, 5,
+				  5, 5, 5, 5, 5, 5,
+				  5, 5, 5, 5, 5, 5,
+				  5, 5, 5, 5, 5, 5], R).
